627a4b230a3c1b3b6cbfb7dc3b54e0a86d32637e
Abhijeet Verma <152282488+Abhij108@users.noreply.github.com>
2025-05-08 17:12:43 +0530

    all stage

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import os    # rename files with the help of os module
-files = os.listdir("cluttred")          # listv the files present in cluttred folder
-i = 1
+# import os    # rename files with the help of os module
+# files = os.listdir("cluttred")          # listv the files present in cluttred folder
+# i = 1
 
-for file in files:
-    if file.endswith(".png"):               # find png files:
-        os.rename(f"cluttred/{file}", f"cluttred/{i}.png")      # os method to rename files :
-        i = i+1
-    else:
-        pass
+# for file in files:
+#     if file.endswith(".png"):               # find png files:
+#         os.rename(f"cluttred/{file}", f"cluttred/{i}.png")      # os method to rename files :
+#         i = i+1
+#     else:
+#         pass
 
+n = int(input("enter a numbeerr"))
+m = int(input("enter a number"))
+
+for numrow in range(n):
+    for numcol in range(m):
+        print(numcol+1, end=" ")
+    print(" ")
